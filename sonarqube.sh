0f0f981743af9654ac33231ee1b767021d557416
Vishnu-chaitanya <vishnuperecharla@outlook.com>
2026-07-31 12:51:30 +0530

    Update SonarQube installation script for version 10.4.1

--- sonarqube.sh
+++ sonarqube.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
-echo "pwd"
+set -e
+
+echo "Current Directory: $(pwd)"
 cd ~
+
+# 1. Update and install dependencies
 sudo apt update -y
-sudo apt install unzip -y
-wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-26.7.0.124771.zip?_gl=1*sp44lq*_gcl_au*MTk2MTI5NDg4NS4xNzg1NDgwMDg3Li0uLS4xNzg1NDgwMDg3LjI2NjkwNzEwMS4xNzg1NDgwMDg3LjE3ODU0ODAyNjg.*_ga*MTQwMDAxNzAxMS4xNzg1NDgwMDg3*_ga_9JZ0GZ5TC6*czE3ODU0ODAwODYkbzEkZzEkdDE3ODU0ODAyODgkajQwJGwwJGgw
-mv sonarqube-26.7.0.124771.zip\?_gl\=1\*h6da8j\*_gcl_au\*MTk2MTI5NDg4NS4xNzg1NDgwMDg3Li0uLS4xNzg1NDgwMDg3LjI2NjkwNzEwMS4xNzg1NDgwMDg3LjE3ODU0ODAyNjg.\*_ga\*MTQwMDAxNzAxMS4xNzg1NDgwMDg3\*_ga_9JZ0GZ5TC6\*czE3ODU0ODwODYkbzEkZzEkdDE3ODU0ODAyODgkajQwJG sonarqube.zip
+sudo apt install unzip openjdk-17-jdk -y
+
+# 2. Download directly to sonarqube.zip (using a clean URL)
+wget -O sonarqube.zip "https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-10.4.1.88267.zip"
+
+# 3. Unzip and rename
 unzip sonarqube.zip
-mv sonarqube-26.7.0.124771 sonarqube
-cd /sonarqube/bin
+mv sonarqube-10.4.1.88267 sonarqube
+
+# 4. Navigate to the correct architecture directory and start
+cd ~/sonarqube/bin/linux-x86-64
 ./sonar.sh start
